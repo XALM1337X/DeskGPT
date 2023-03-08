b8025cb6db4f6bfc2c4d010398b8b6b3d3a84e8e
logan <loganmitchell2011@gmail.com>
2023-03-08 11:21:49 -0500

    changes and notes.

--- src/DeskGPT.cpp
+++ src/DeskGPT.cpp
@@ -6,12 +6,18 @@
 #include <regex>
 
 int main(int argc, char **argv) {
+    //TODO: Rewrite this process in a loop that waits for user input and will use the conversations session-id to carry on context with the chat model.
+    //-H 'Session-Id: <your_session_id>'
     std::string pattern = "^.*\n+(.*)$";
     std::regex regex(pattern);
     std::smatch match;
     std::string key = std::string(argv[1]);
     std::string arg = std::string(argv[2]);    
-    std::string command =  "curl -s https://api.openai.com/v1/completions -H \"Content-Type: application/json\" -H \"Authorization: Bearer "+key+"\" -d '{\"model\": \"text-davinci-003\", \"prompt\": \""+arg+"\", \"temperature\": 0, \"max_tokens\": 2048}'";
+    std::string command =  "curl -s https://api.openai.com/v1/chat/completions \
+    -H \"Authorization: Bearer "+key+"\" \
+    -H \"Content-Type: application/json\" \
+    -d '{ \"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""+arg+"\"}]}'";
+    
     FILE* pipe = popen(command.c_str(),"r");  
     if (!pipe) {
         std::cout << "Failed to open pipe" << std::endl;
@@ -26,9 +32,8 @@ int main(int argc, char **argv) {
             result_str += buffer;
         }            
     }
-
-    pclose(pipe);
-    
+    //printf("%s", result_str.c_str());
+    pclose(pipe);    
     rapidjson::Document document;
     rapidjson::ParseResult result = document.Parse(result_str.c_str());
     if (!result) {
@@ -40,16 +45,18 @@ int main(int argc, char **argv) {
     if (choices.IsArray()) {
         const rapidjson::Value& choice = choices[0];
         if (choice.IsObject()) {
-            const rapidjson::Value& text = choice["text"];
-            if (text.IsString()) {
-                std::string str = std::string(text.GetString());
-                if (std::regex_match(str, match, regex)) {
-                    std::cout << match[1] << std::endl;
-                } else {
-                    std::cout << text.GetString() << std::endl;
+            const rapidjson::Value& message = choice["message"];
+            if (message.IsObject()) {
+                const rapidjson::Value& content = message["content"];
+                if (content.IsString()) {
+                    std::string str = std::string(content.GetString());
+                    if (std::regex_match(str, match, regex)) {
+                        std::cout << match[1] << std::endl;
+                    } else {
+                        std::cout << content.GetString() << std::endl;
+                    }
                 }
             }
         }
     }
 }
-
