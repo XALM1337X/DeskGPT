2b5ceac65bc5d104c032c940460619dab0a1bd72
logan <loganmitchell2011@gmail.com>
2023-03-11 21:58:03 -0500

    added core_server header file

---
+++ src/include/CoreServer.h
@@ -0,0 +1,15 @@
+#ifndef _CORE_SERVER_H_
+#define _CORE_SERVER_H_
+//#include <string>
+class CoreServer {
+    public:
+        CoreServer();
+        void Init();
+    private:
+
+    public:
+    private:
+
+};
+
+#endif
