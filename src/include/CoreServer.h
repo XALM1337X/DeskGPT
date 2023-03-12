2fc7a59b6a36fa48c2bba1e0e8cdecf35f1b7ba2
logan <loganmitchell2011@gmail.com>
2023-03-12 18:56:24 -0400

    added definition and methods for starting and running server core.

--- src/include/CoreServer.h
+++ src/include/CoreServer.h
@@ -1,13 +1,24 @@
 #ifndef _CORE_SERVER_H_
 #define _CORE_SERVER_H_
 //#include <string>
+#include <sys/socket.h> // for sockets
+#include <arpa/inet.h> // for internet addresses
+#include <unistd.h> // for close() function
 class CoreServer {
     public:
         CoreServer();
+        ~CoreServer();
         void Init();
+        void StartAcceptHandler();
+        void StopAcceptHandler();
+        void AcceptHandler();
     private:
 
     public:
+        int server_fd;
+        int opt;
+        struct sockaddr_in address;
+        bool* isRunning;
     private:
 
 };
