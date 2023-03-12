98133d0411eb2c4f57410b4bd8252f8d4788c9f1
logan <loganmitchell2011@gmail.com>
2023-03-12 18:54:17 -0400

    added Init function for CoreServer

--- src/CoreServer.cpp
+++ src/CoreServer.cpp
@@ -18,5 +18,18 @@ CoreServer::~CoreServer() {
 
 
 void CoreServer::Init() {
-    //std::cout << "Init-Test:" << std::endl;
+    //TODO: This is linux boilerplate that needs to be implemented for server
+    this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    this->opt = 1;
+    setsockopt(this->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt));    
+    this->address.sin_family = AF_INET;
+    this->address.sin_addr.s_addr = INADDR_ANY;
+    this->address.sin_port = htons(8080);    
+    bind(this->server_fd, (struct sockaddr *)&this->address, sizeof(this->address));
+    listen(this->server_fd, 3);
+    *this->isRunning = true;
+    this->StartAcceptHandler();    
+}
+
+
 }
