bb22d5bc1fbafa808aad4a007bcd88a5a04ff907
logan <loganmitchell2011@gmail.com>
2023-03-13 22:55:52 -0400

    massive refactor, moved pretty much everything outside loop into packages.

--- src/GPTMobileServer.cpp
+++ src/GPTMobileServer.cpp
@@ -15,31 +15,6 @@
 
 int main(int argc, char **argv) {    
     Core* DeskGPTCore = new Core();    
-    std::string user_in = "";
-    std::cout <<"Question: ";
-    while (*DeskGPTCore->CoreServerObj->isRunning == true) {
-        while ( std::getline(std::cin, user_in)) {        
-            if (user_in == "--exit") {
-                std::cout << "shutting down" << std::endl;
-                *DeskGPTCore->CoreServerObj->isRunning = false;
-                break;
-            } else if (user_in == "--clear" || user_in == "-c") {
-                system("clear");
-                std::cout <<"Question: ";
-                continue;
-            } else if (user_in == "--help" || user_in == "-h") {
-                Core::DisplayHelp();
-                std::cout <<"Question: ";
-                continue;
-            } else {
-                DeskGPTCore->SetCommand(user_in);
-                DeskGPTCore->ExecuteCommand();
-            }        
-            user_in = "";
-            std::cout <<"Question: ";
-            
-        }
-    }
-
+    while (DeskGPTCore->CoreServerObj->isRunning){}
     delete(DeskGPTCore);    
 }
