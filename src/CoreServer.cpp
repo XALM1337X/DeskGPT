3f17d0295325d8b35d9c516198c5a47aaf4f0205
logan <loganmitchell2011@gmail.com>
2023-03-13 23:01:04 -0400

    changed to use atomic bool and added a block to init for reading API key for server.

--- src/CoreServer.cpp
+++ src/CoreServer.cpp
@@ -15,10 +15,10 @@ CoreServer::~CoreServer() {
     delete(this->isRunning);
 }
 
-
-
 void CoreServer::Init() {
-    //TODO: This is linux boilerplate that needs to be implemented for server
+    if (!this->ReadAPIKey()){
+        return;
+    }    
     this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
     this->opt = 1;
     setsockopt(this->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt));    
@@ -27,7 +27,7 @@ void CoreServer::Init() {
     this->address.sin_port = htons(8080);    
     bind(this->server_fd, (struct sockaddr *)&this->address, sizeof(this->address));
     listen(this->server_fd, 3);
-    *this->isRunning = true;
+    this->isRunning = true;
     this->StartAcceptHandler();    
 }
 
