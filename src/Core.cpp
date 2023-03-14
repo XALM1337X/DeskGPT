fad61f9c68d3e9b8a790c816a51a4ef8e370a946
logan <loganmitchell2011@gmail.com>
2023-03-13 22:42:54 -0400

    massive refactor, moved a lot fo stuff into CoreServer.

--- src/Core.cpp
+++ src/Core.cpp
@@ -1,17 +1,9 @@
 #include "Core.h"
-#include <string>
-#include <iostream>
-#include "rapidjson/document.h"
-#include <regex>
 #include "log.h"
-#include <fstream>
 #include "CoreServer.h"
 
 Core::Core() {
     MainLog::WriteLog("Core::~Core: Initializing...");
-    this->current_command = "";
-    this->api_key = "";
-    this->context_id = "";
     this->CoreServerObj = new CoreServer();
     this->Init();
 }
@@ -21,100 +13,7 @@ Core::~Core() {
     delete(this->CoreServerObj);
 }
 void Core::Init() {
-    this->ReadAPIKey();
-}
-
-
-void Core::ReadAPIKey() {
-    std::ifstream file("/root/GPTMobileServer/src/etc/api.key");
-    if (!file.is_open()) {
-        ErrorLog::WriteLog("Core::Init:error - Failed opening file");
-    } else {
-        std::string line ="";
-        std::getline(file,line);
-        this->api_key = line;
-    }
-}
-
-void Core::DisplayHelp() {
- std::string help_str = ("NAME:\n"  
-            "  DeskGPT\n"
-            "USAGE:\n" 
-            "  DeskGPT.exe [options] <api_key>\n"
-            "OPTIONS:\n"
-            "  --help or -h     To see this option screen\n"
-            "  --clear or -c    To Clear the screen\n"
-            "  --exit           To shutdown program\n");
-    std::cout << help_str <<std::endl;
-}
-
-void Core::SetCommand(std::string command_str) {
-    std::string command = "";
-    if (this->context_id != "") {
-        command =  "curl -s https://api.openai.com/v1/chat/completions \
-        -H \"Session-Id: "+this->context_id+"\" \
-        -H \"Authorization: Bearer "+this->api_key+"\" \
-        -H \"Content-Type: application/json\" \
-        -d '{ \"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""+command_str+"\"}]}'";
-    } else {
-        command =  "curl -s https://api.openai.com/v1/chat/completions \
-        -H \"Authorization: Bearer "+this->api_key+"\" \
-        -H \"Content-Type: application/json\" \
-        -d '{ \"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \""+command_str+"\"}]}'";
-    }
-
-    this->current_command = command;
-}
-
-std::string Core::GetCommandString() {
-    return this->current_command;
-}
-
-void Core::ExecuteCommand() {       
-    char buffer[128];
-    std::string result_str = "";
-    std::smatch match;
-    std::string pattern = "^.*\n+(.*)$";
-    std::regex regex(pattern);
-    FILE* pipe = popen(this->GetCommandString().c_str(),"r");  
-    if (!pipe) {
-        ErrorLog::WriteLog("Core::ExecuteCommand:error - Failed to open pipe");
-    }
-    while (!feof(pipe)) {
-        if (fgets(buffer, 128, pipe) != NULL){
-            result_str += buffer;
-        }            
-    }
-    pclose(pipe);    
-    rapidjson::Document document;
-    rapidjson::ParseResult result = document.Parse(result_str.c_str());
-    if (!result) {
-        ErrorLog::WriteLog("Core::ExecuteCommand:error - Error parsing JSON");
-    } else {
-        const rapidjson::Value& choices = document["choices"];
-        if (choices.IsArray()) {
-            const rapidjson::Value& choice = choices[0];
-            if (choice.IsObject()) {
-                const rapidjson::Value& message = choice["message"];
-                if (message.IsObject()) {
-                    const rapidjson::Value& content = message["content"];
-                    if (content.IsString()) {
-                        std::cout << std::endl;
-                        std::string str = std::string(content.GetString());
-                        if (std::regex_match(str, match, regex)) {
-                            std::cout << match[1] << std::endl;
-                        } else {
-                            std::cout << str << std::endl;
-                        }
-                    }
-                }
-            }
-        }
-        const rapidjson::Value& id = document["id"];
-        if (id.IsString()) {
-            this->context_id = id.GetString();
-        }
-    }
+    
 }
 
 
--- src/include/Core.h
+++ src/include/Core.h
@@ -9,23 +9,13 @@ class Core {
     public:
         Core();
         ~Core();
-        static void DisplayHelp();
-        void SetCommand(std::string);
-        std::string GetCommandString();
-        void ReadAPIKey();
-        void ExecuteCommand();
         void Init();
         void CleanUp();
     private:
 
     //Members
-    public:
-
-        
+    public:        
         CoreServer* CoreServerObj;
-        std::string current_command;
-        std::string api_key;
-        std::string context_id;
     private:
 };
 #endif
