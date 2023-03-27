945a4679dbde3a36dd9cc3291483f35d7ff79eae
logan <loganmitchell2011@gmail.com>
2023-03-27 18:19:36 -0400

    Implemented a sleep on the main thread to stop excessive CPU use.

--- src/GPTMobileServer.cpp
+++ src/GPTMobileServer.cpp
@@ -1,15 +1,21 @@
+#include "Core.h"
+#include "CoreServer.h"
 #include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include "Core.h"
-#include "CoreServer.h"
+#include <chrono>
+#include <thread>
+
 //For Sockets in windows
 //#include <WinSock2.h> // for sockets
 //#include <Ws2tcpip.h> // for internet addresses
 
 int main(int argc, char **argv) {    
     Core* DeskGPTCore = new Core();    
-    while (DeskGPTCore->CoreServerObj->isRunning){}
+        
+    while (DeskGPTCore->CoreServerObj->isRunning){
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    }
     delete(DeskGPTCore);    
 }
