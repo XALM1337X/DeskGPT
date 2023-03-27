ea248108a19d7b81f209c62ff43693991172a38d
logan <loganmitchell2011@gmail.com>
2023-03-27 02:28:09 -0400

    changed http test string.

--- src/GPTMobileClientTest.cpp
+++ src/GPTMobileClientTest.cpp
@@ -34,17 +34,17 @@ int main(int argc, char const *argv[]) {
             system("clear");
             skip = true;
         } else if (user_input == "--test-http") {
-            std::string http_msg = "POST /cgi-bin/process.cgi HTTP/1.1\r\n"
-                                   "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\n"
-                                   "Host: www.tutorialspoint.com\r\n"
-                                   "Content-Type: text/xml; charset=utf-8\r\n"
-                                   "Content-Length: length\r\n"
-                                   "Accept-Language: en-us\r\n"
-                                   "Accept-Encoding: gzip, deflate\r\n"
-                                   "Connection: Keep-Alive\r\n"
-                                   "\r\n"
-                                   "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n"
-                                   "<string xmlns=\"http://clearforest.com/\">string</string>\r\n";
+            std::string http_msg = "POST /cgi-bin/process.cgi HTTP/1.1\n"
+                                   "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n"
+                                   "Host: www.tutorialspoint.com\n"
+                                   "Content-Type: text/xml; charset=utf-8\n"
+                                   "Content-Length: length\n"
+                                   "Accept-Language: en-us\n"
+                                   "Accept-Encoding: gzip, deflate\n"
+                                   "Connection: Keep-Alive\n"
+                                   "\n"
+                                   "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                                   "<string xmlns=\"http://clearforest.com/\">string</string>\n";
             send(clientSocket, http_msg.c_str(), strlen(http_msg.c_str()), 0);
         } else {
             send(clientSocket, user_input.c_str(), strlen(user_input.c_str()), 0);
